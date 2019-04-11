f6980fb86ce6ed27c7ea5cf015b952c3eb3fe5c6
ariannmichael <chael.andrade@gmail.com>
2019-04-11 15:19:28 -0300

    Footer and Profile Photo

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './SignUpPage.css';
+import Footer from './../components/footer/Footer'
 import canarinho from './../assets/images/canarinho.svg';
+import profilePhoto from './../assets/images/cutmypic.png';
 
 const SignUpPage = (props) => {
     return (
         <div className="SignUp-page">
             <div className="Logo-side">
-                <div class="SignUp-text">Sign up for Canarinho</div>
+                <div className="SignUp-text">Sign up for Canarinho</div>
+                <img src={profilePhoto} className="Profile-photo" alt="photo"/>
                 <img src={canarinho} className="Canarinho-logo" alt="logo" />
                 <div className="Logo-title">Canarinho</div>
+            </div>
+            <div className="footer">
+                <Footer/>
             </div>
         </div>
     );
