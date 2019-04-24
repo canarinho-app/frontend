d65824e175011d5ae5554b6f5e9bf9eddeda216d
Victor Borges <victor.araujo@ccc.ufcg.edu.br>
2019-04-24 20:23:36 -0300

    Adding axios and fixes in SignUpPage

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -12,41 +12,39 @@ const SignUpPage = (props) => {
             <Row>
                 <Col xs={6} md={3} className="logo-side">
                     <div className="canarinho-logo">
-                        <Image src={canarinho} alt="logo" fluid/> 
+                        <Image src={canarinho} alt="logo" fluid />
                         <div className="signup-logo-title">Canarinho</div>
                     </div>
                 </Col>
-                <Col xs={6} md={9}>
+                <Col xs={6} md={9} className="form-box">
                     <div className="text-center">
                         <div className="signup-text">Sign up for Canarinho</div>
-                        <Image src={profilePhoto} className="profile-photo" alt="photo"/>
+                        <Image src={profilePhoto} className="profile-photo" alt="photo" />
                         <div>
                             <a href="" className="select-photo">Select your profile photo</a>
                         </div>
                     </div>
-                    
+
                     <Form>
                         <Row className="justify-content-md-center no-margin signup-input-box">
-                    
-                                <Form.Control className="signup-input" size="lg" placeholder="Username"/>
-                                
-                                <Form.Control className="signup-input" size="lg" placeholder="Display name"/>
-                            
-                                <Form.Control className="signup-input" size="lg" placeholder="Email"/>
-                            
-                                <Form.Control className="signup-input" size="lg" placeholder="Password" type="password"/>
-                            
-                                <Form.Control className="signup-input" size="lg" placeholder="Repeat your password" type="password"/>
-                            
 
-                                <Button className="signup-button" variant="primary" size="lg">Sign up</Button> 
+                            <Form.Control className="signup-input" size="lg" placeholder="Username" />
+
+                            <Form.Control className="signup-input" size="lg" placeholder="Display name" />
+
+                            <Form.Control className="signup-input" size="lg" placeholder="Email" />
+
+                            <Form.Control className="signup-input" size="lg" placeholder="Password" type="password" />
+
+                            <Form.Control className="signup-input" size="lg" placeholder="Repeat your password" type="password" />
+
+
+                            <Button className="signup-button" variant="primary" size="lg">Sign up</Button>
                         </Row>
                     </Form>
                 </Col>
             </Row>
-            <div className="footer">
-                <Footer/>
-            </div>
+            <Footer />
         </div>
     );
 }
