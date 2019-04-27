181bb9a489161f0ba8f0c6323f58d01dd929efce
ariannmichael <ariann.farias@ccc.ufcg.edu.br>
2019-04-26 21:30:32 -0300

    Navbar center aligned

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Navbar.css';
-import { Link } from 'react-router-dom';
-import { Image } from 'react-bootstrap';
+import { Image, ButtonToolbar, Button } from 'react-bootstrap';
 
 import canarinho from '../../assets/images/canarinho.svg';
 import profilePhoto from '../../assets/images/cutmypic.png';
@@ -10,12 +9,14 @@ const Navbar = (props) => {
     return (
         <aside class="menu-area">
             <nav className="menu">
-                <Link to="/" className="home-navbar">Home</Link>
-                <Link to="/notifications" className="notification-navbar">Notifications</Link>
-                <Image src={canarinho} className="logo-navbar" alt="logo" fluid/> 
-                <i className="fas fa-search"></i>
-                <text className="search-navbar">Search</text>
-                <Image src={profilePhoto} className="profile-photo-navbar" alt="photo"/>
+                <ButtonToolbar>
+                    <Button href="/" className="home-navbar" variant="outline-none">Home</Button>
+                    <Button href="/notification" className="notification-navbar" variant="outline-none">Notifications</Button>
+                    <Image src={canarinho} className="logo-navbar" alt="logo" fluid/> 
+                    <i className="fas fa-search"></i>
+                    <Button className="search-navbar" variant="outline-none">Search</Button>
+                    <Image src={profilePhoto} className="profile-photo-navbar" alt="photo"/>
+                </ButtonToolbar>
             </nav>
         </aside>
 
