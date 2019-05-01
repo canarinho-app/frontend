1c232cc71d0c5e69c5c4db07a357a10d9daee8bf
Victor Borges <victor.araujo@ccc.ufcg.edu.br>
2019-05-01 20:37:39 -0300

    PostCard component. Feed & Navbar fixes

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,9 +1,10 @@
 import React from "react";
 import './Feed.css';
-import { Button, Image, Form, Row, Col } from 'react-bootstrap';
+import { Button, Image, Row, Col } from 'react-bootstrap';
 
 import Navbar from './../components/navbar/Navbar';
 import profilePhoto from './../assets/images/cutmypic.png';
+import PostCard from './../components/post/PostCard';
 
 const Feed = () => {
     return (
@@ -13,7 +14,6 @@ const Feed = () => {
                 <Row className="justify-content-md-center no-margin">
                     <Col md="auto">
                             <div className="content-box">
-                                
                                     <Row className="justify-content-md-center align-items-center no-margin login-input-box profile-info">
                                         <Col xs="4">
                                             <Image src={profilePhoto} className="profile-info-photo" alt="photo" />
@@ -25,12 +25,12 @@ const Feed = () => {
                                             <Col className="no-padding username-text">
                                                 <div>@victorborges</div>
                                             </Col>
-                                            <Col className="no-padding">
+                                            <Col>
                                                 <Row>
-                                                    <Col xs="2" className="fol-counter fol-counter-number">500</Col>
-                                                    <Col xs="3" className="fol-counter fol-counter-label">Following</Col>
-                                                    <Col xs="2" className="fol-counter fol-counter-number fol-counter-number-margin">501</Col>
-                                                    <Col xs="3" className="fol-counter fol-counter-label">Followers</Col>
+                                                    <span className="fol-counter-number">500</span>
+                                                    <span className="fol-counter-label">Following</span>
+                                                    <span className="fol-counter-number">501</span>
+                                                    <span className="fol-counter-label">Followers</span>
                                                 </Row>
                                             </Col>
                                         </Col>
@@ -38,7 +38,14 @@ const Feed = () => {
                                             <Button className="edit-profile-button" variant="primary" size="lg">Edit Profile</Button>
                                         </Col>
                                     </Row>
-                                
+                                    <PostCard/>
+                                    <PostCard/>
+                                    <PostCard/>
+                                    <PostCard/>
+                                    <PostCard/>
+                                    <PostCard/>
+                                    <PostCard/>
+                                    <PostCard/>
                             </div>
                     </Col>
                 </Row>
