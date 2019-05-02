9ec364cd893d0d5e0e4ef580068a28f186f1b5cb
ariannmichael <ariann.farias@ccc.ufcg.edu.br>
2019-05-02 16:06:26 -0300

    LoginPage if isLoggedIn false

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,11 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Navbar from '../navbar/Navbar';
 import Routes from '../../Routes';
+import LoginPage from '../../pages/LoginPage'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {isLoggedIn: true};
+    this.state = {isLoggedIn: false};
   }
 
   render() {
@@ -16,7 +17,7 @@ class App extends Component {
       <BrowserRouter>
           <Routes/>
         <div className="app">
-          {isLoggedIn && (<Navbar/>) }
+          {isLoggedIn ? (<Navbar/>) : (<LoginPage/>)}
         </div>
       </BrowserRouter>
     );
