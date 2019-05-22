4db75f9e78c63c559460c15c3ba0ba60af5c1431
ariannmichael <ariann.farias@ccc.ufcg.edu.br>
2019-05-22 20:00:18 -0300

    Navbar css changed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,8 +4,7 @@ import { Container, Image, ButtonToolbar, Button, Navbar, Nav, Form } from 'reac
 import { Link } from 'react-router-dom';
 import canarinho from '../../assets/images/canarinho.svg';
 import defaultPhoto from '../../assets/images/cutmypic.png';
-import SearchPage from '../../pages/SearchPage';
-import { Redirect } from 'react-router';
+
 
 const uploads = 'http://localhost:3001/uploads/';
 
@@ -26,12 +25,15 @@ class NavbarCanarinho extends Component {
             <Container>
                 <Navbar className="menu-area" fixed="top">
                     <Nav className="menu">
-                        <Image src={canarinho} className="logo-navbar" alt="logo" fluid />
                         <ButtonToolbar>
                             <Link to={{ pathname: '/home', state: { isLoggedIn: true, username: this.props.user.username } }} className="home-navbar" variant="outline-none">
                                 Home
                             </Link>
-                            <Button className="notification-navbar" variant="outline-none">Notifications</Button>
+                            <Link className="notification-navbar" variant="outline-none">Notifications</Link>
+                            <Link to={{ pathname: '/login', state: { isLoggedIn: false} }} className="logout-navbar" variant="outline-none">
+                                Logout
+                            </Link>
+                            <Image src={canarinho} className="logo-navbar" alt="logo" fluid />
                             <Form.Control className="search-form " type="text" onChange={this.handleChangeSearch} />
                             <Link to={{ pathname: '/search', state: {search: this.state.search, user: this.props.user} }} className="search-navbar" variant="outline-none"
                                 >
@@ -40,9 +42,6 @@ class NavbarCanarinho extends Component {
                             </Link>
                             <Link to={{ pathname: '/profile', state: this.props.user }}>
                                 <Image src={this.props.user.profileImg ? `${uploads}${this.props.user.profileImg}` : defaultPhoto} className="profile-photo-navbar" alt="photo" />
-                            </Link>
-                            <Link to={{ pathname: '/login', state: { isLoggedIn: false} }} className="logout-navbar" variant="outline-none">
-                                Logout
                             </Link>
                         </ButtonToolbar>
                     </Nav>
